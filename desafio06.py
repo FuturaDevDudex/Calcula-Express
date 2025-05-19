c93a11f8524137b3b12425341db182847727f470
Eduarda Timóteo <eduarda.silva7739@gmail.com>
2025-05-19 20:07:26 -0300

    Add files via upload

---
+++ desafio06.py
@@ -0,0 +1,5 @@
+n = int(input('Me informe o seu número da sorte: '))
+
+print(f'O dobro do número {n} é {n*2}')
+print(f'o triplo do número {n} é {n*3}')
+print (f'A raiz quadrada de{n} é {n**0.5:.3f}')
